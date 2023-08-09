50efc8796e0f26e8ae0a18d2c9a2e1c4a7916a11
ftaufik.ros <ftaufik.ros@gmail.com>
2023-08-09 12:27:29 +0700

    adjust config

--- config/db.config.js
+++ config/db.config.js
@@ -1,16 +1,21 @@
+const Sequelize = require('sequelize');
 require("dotenv").config();
 // DATABASE SETUP
-module.exports = {
-    HOST: process.env.DB_HOST,
-    USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASSWORD,
-    DB: process.env.DB_NAME,
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-};
+// module.exports = {
+//     HOST: process.env.DB_HOST,
+//     USER: process.env.DB_USER,
+//     PASSWORD: process.env.DB_PASSWORD,
+//     DB: process.env.DB_NAME,
+//     dialect: "postgres",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+// };
+
+const db = new Sequelize(process.env.DB_URI)
+
+module.exports = db
 

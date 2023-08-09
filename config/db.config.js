ad023aa60d98f93119e86c62974ba4d1d2126006
ftaufik.ros <ftaufik.ros@gmail.com>
2023-08-09 19:49:26 +0700

    back to default

--- config/db.config.js
+++ config/db.config.js
@@ -2,17 +2,11 @@ const Sequelize = require('sequelize');
 require("dotenv").config();
 // DATABASE SETUP
 module.exports = {
-    HOST: "containers-us-west-175.railway.app",
-    USER: "postgres",
-    PASSWORD: "Ejo3sHT8tUIlEPfhG1Fy",
-    DB: "railway",
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
     dialect: "postgres",
-     dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
     pool: {
       max: 5,
       min: 0,
@@ -20,20 +14,4 @@ module.exports = {
       idle: 10000
     }
 };
-// module.exports = {
-//     HOST: process.env.DB_HOST,
-//     USER: process.env.DB_USER,
-//     PASSWORD: process.env.DB_PASSWORD,
-//     DB: process.env.DB_NAME,
-//     dialect: "postgres",
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000
-//     }
-// };
-
-
-// module.exports = db
 
